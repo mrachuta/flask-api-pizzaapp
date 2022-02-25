729b4e3ee46797cdde22bb99fa1a4e5ff721dae7
Mateusz Rachuta <mrachuta@thinkbox.pl>
2022-02-25 20:21:00 +0100

    Add Dockerfile and required resources; add new stage in pipeline

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Skip DB migration if variable FLASK_SKIP_MIGRATION is set
+if [[ -z "${FLASK_SKIP_DB_MIGRATION}" ]]; then
+  echo "INFO: Running DB migrations..."
+  python manage.py db init
+  python manage.py db migrate
+  python manage.py db upgrade
+fi
+
+echo "INFO: Starting application..."
+python manage.py runserver
