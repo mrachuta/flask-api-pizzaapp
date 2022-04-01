cde66b80d061eda3dc5fdf3ae33bc271c15a6c42
Mateusz Rachuta <mrachuta@thinkbox.pl>
2022-04-01 19:58:28 +0200

    Add CSRF protection because of SQ rule

--- pizzaapp/app.py
+++ pizzaapp/app.py
@@ -4,9 +4,11 @@ from .config import app_config
 
 # Import of model is necessary
 from .models import db, pizza_model
+from flask_wtf.csrf import CSRFProtect
 from flask import render_template, url_for
 from .views.pizza_view import pizza_api as pizza_blueprint
 
+csrf = CSRFProtect()
 
 def create_app(env_name):
 
@@ -14,10 +16,10 @@ def create_app(env_name):
     Create app
     """
 
-    app = Flask(__name__, template_folder="templates")
-
+    app = Flask(__name__, template_folder="templates", static_folder="static")
     app.config.from_object(app_config[env_name])
     db.init_app(app)
+    csrf.init_app(app)
     app.register_blueprint(pizza_blueprint, url_prefix="/api/v1/pizza")
 
     @app.route("/", methods=["GET"])
