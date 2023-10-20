11bd1ff660bdccccf6008710f17665a134721b48
mrachuta <34251394+mrachuta@users.noreply.github.com>
2023-10-20 09:37:15 +0200

    Another fix for rule

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -16,47 +16,47 @@ resource "azurerm_container_registry" "acr" {
   )
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-  dns_prefix          = var.aks_name
-  sku_tier            = "Free"
-  node_resource_group = var.aks_resources_rg_name
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = var.aks_name
+#   resource_group_name = data.azurerm_resource_group.rg.name
+#   location            = data.azurerm_resource_group.rg.location
+#   dns_prefix          = var.aks_name
+#   sku_tier            = "Free"
+#   node_resource_group = var.aks_resources_rg_name
 
-  network_profile {
-    network_plugin    = "kubenet"
-    load_balancer_sku = var.aks_lb_sku
-  }
+#   network_profile {
+#     network_plugin    = "kubenet"
+#     load_balancer_sku = var.aks_lb_sku
+#   }
 
-  dynamic "api_server_access_profile" {
-    for_each = (
-      var.aks_lb_sku == "standard" && var.aks_auth_ip_ranges != null
-    ) ? [1] : []
-    content {
-      authorized_ip_ranges = [
-        var.aks_auth_ip_ranges
-      ]
-    }
-  }
+#   dynamic "api_server_access_profile" {
+#     for_each = (
+#       var.aks_lb_sku == "standard" && var.aks_auth_ip_ranges != null
+#     ) ? [1] : []
+#     content {
+#       authorized_ip_ranges = [
+#         var.aks_auth_ip_ranges
+#       ]
+#     }
+#   }
 
-  default_node_pool {
-    name                        = "default"
-    temporary_name_for_rotation = "defaulttemp"
-    node_count                  = 1
-    vm_size                     = "Standard_B2s"
-    os_disk_size_gb             = 32
-    os_disk_type                = "Managed"
-  }
+#   default_node_pool {
+#     name                        = "default"
+#     temporary_name_for_rotation = "defaulttemp"
+#     node_count                  = 1
+#     vm_size                     = "Standard_B2s"
+#     os_disk_size_gb             = 32
+#     os_disk_type                = "Managed"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  tags = merge(
-    {
-      "managed_by" = "terraform"
-    },
-    var.extra_tags
-  )
-}
+#   tags = merge(
+#     {
+#       "managed_by" = "terraform"
+#     },
+#     var.extra_tags
+#   )
+# }
