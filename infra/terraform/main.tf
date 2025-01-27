245e9260756d7ef5a0f9dce5bf7051829becfc00
Mateusz Rachuta <34251394+mrachuta@users.noreply.github.com>
2025-01-27 23:35:47 +0100

    Add missing terraform variable (#44)
    * Add version label to pod template labels
    
    * Add missing terraform variable; update version in Chart.yaml

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,6 +5,7 @@ module "aks_cheap_cluster" {
   provision_acr                 = var.provision_acr
   acr_custom_region             = var.acr_custom_region
   provision_aks                 = var.provision_aks
+  aks_custom_region             = var.aks_custom_region
   acr_name                      = var.acr_name
   acr_grant_pull_role_to_aks    = true
   aks_name                      = var.aks_name
