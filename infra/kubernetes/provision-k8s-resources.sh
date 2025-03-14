857486060a7aa84fe4a6a7c0793755257a476d19
Mateusz Rachuta <34251394+mrachuta@users.noreply.github.com>
2025-03-14 21:51:21 +0100

    Update infra resources (#46)
    * Update bash script
    
    * Update aks-cheap-cluster module to latest version
    
    * Update pipelines
    
    * Update provider version and module version
    
    * Update tflint config
    
    * Update tenant variable
    
    * Update variable
    
    * Remove certificate annotation
    
    * Include checkov in image
    
    * Update variable name
    
    * Update variables
    
    * Update release pipieline's variables
    
    * Fix issues with missing cert
    
    * Update tf module version

--- infra/kubernetes/provision-k8s-resources.sh
+++ infra/kubernetes/provision-k8s-resources.sh
@@ -2,11 +2,18 @@
 
 set -euo pipefail
 
-CERT_MANAGER_VER='v1.13.1'
-#NGINX_INGRESS_VER='v1.8.2'
+CERT_MANAGER_VER='v1.17.0'
+# No loger required in script
+#NGINX_INGRESS_VER='v1.12.0'
 
 echo 'Creating namespaces'
 kubectl apply -f ./namespaces.yaml
+
+# # No longer required as it is handled by terraform
+# echo "Applying ingress-nginx version ${NGINX_INGRESS_VER}"
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-${NGINX_INGRESS_VER}/deploy/static/provider/cloud/deploy.yaml
+# echo 'Sleep for 30 seconds'
+# sleep 30
 
 echo "Applying cert manager version ${CERT_MANAGER_VER}"
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${CERT_MANAGER_VER}/cert-manager.yaml
@@ -21,12 +28,6 @@ kubectl apply -f ./letsencrypt-staging-clusterissuer.yaml
 # kubectl apply -f ./acr-registry-secret-dev-env.yaml 
 # kubectl apply -f ./acr-registry-secret-uat-env.yaml
 # kubectl apply -f ./acr-registry-secret-prod-env.yaml
-
-# # No longer required as it is handled by terraform and helm provider
-# echo "Applying ingress-nginx version ${NGINX_INGRESS_VER}"
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-${NGINX_INGRESS_VER}/deploy/static/provider/cloud/deploy.yaml
-# echo 'Sleep for 30 seconds'
-# sleep 30
 
 echo 'Getting loadBalancer IP address...'
 until kubectl get svc ingress-nginx-controller -n ingress-nginx \
